8ed7841e4556614b004e8e22579e21dbeef50790
tocomon <tocomon09@gmail.com>
2023-07-12 14:01:35 +0900

    init

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center" data-testid="footer">
+    {/* <div className="logo" data-testid="footer-logo" /> */}
+    <p data-testid="footer-text">
+      This is footer
+      {/* Sample project provided by <a href="https://auth0.com">Auth0</a> */}
+    </p>
+  </footer>
+);
+
+export default Footer;
